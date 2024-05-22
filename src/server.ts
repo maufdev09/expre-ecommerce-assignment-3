48aff4b2d2fac65542e14fb7c2710ec55c4fb825
maufdev09 <marufdev09@gmail.com>
2024-05-22 17:00:45 +0600

    initialized the application and setup mongose eslint and server

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+
+
+
+
+async function main() {
+
+try {
+
+
+
+  await mongoose.connect(config.db_url as string);
+  const port = config.port 
+  
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  }
+  )
+
+  
+} catch (error) {
+  console.log(error);
+  
+}
+
+
+
+}
+
+main();
